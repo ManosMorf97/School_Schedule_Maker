c9ce0fb558ed691180e851d322c4f548ae777250
ManosMorf97 <manosmorf97@gmail.com>
2021-03-18 19:12:06 +0200

    Async

--- app/src/main/java/com/example/myapplication/IQActivity.java
+++ app/src/main/java/com/example/myapplication/IQActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,23 +14,46 @@ import android.widget.ProgressBar;
 
 
 public class IQActivity extends AppCompatActivity {
-
+    Button Schedule;
+    EditText etext;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iq);
-        Button Schedule=findViewById(R.id.button6);
-        EditText etext=findViewById(R.id.editTextTextPersonName10);
-        ProgressBar progressBar=findViewById(R.id.progressBar1);
+        Schedule=findViewById(R.id.button6);
+        etext=findViewById(R.id.editTextTextPersonName10);
+        progressBar=findViewById(R.id.progressBar1);
         progressBar.setVisibility(View.GONE);
 
         Schedule.setOnClickListener(view -> {
+           Async execute=new Async();
+           execute.execute();
             progressBar.setVisibility(View.VISIBLE);
             String IQ=etext.getText().toString();
             DataBase.setSchedule(Utilities.Schedule(DataBase.Lessons(),DataBase.Teachers(),IQ));
+
+        });
+    }
+    @SuppressLint("StaticFieldLeak")
+    private final class Async extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String IQ = etext.getText().toString();
+            DataBase.setSchedule(Utilities.Schedule(DataBase.Lessons(), DataBase.Teachers(), IQ));
+            return null;
+        }
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
             Intent activityChangeIntent = new Intent(IQActivity.this, Final_Activity.class);
             startActivity(activityChangeIntent);
-        });
+            progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 
 }
