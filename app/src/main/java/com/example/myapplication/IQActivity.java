38f3c6439e7288ab44479b795a53a7ae4a1acc33
ManosMorf97 <manosmorf97@gmail.com>
2021-03-18 19:29:32 +0200

    Commit

--- app/src/main/java/com/example/myapplication/IQActivity.java
+++ app/src/main/java/com/example/myapplication/IQActivity.java
@@ -1,35 +1,29 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+
 
 
 public class IQActivity extends AppCompatActivity {
     Button Schedule;
     EditText etext;
-    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iq);
         Schedule=findViewById(R.id.button6);
         etext=findViewById(R.id.editTextTextPersonName10);
-        progressBar=findViewById(R.id.progressBar1);
-        progressBar.setVisibility(View.GONE);
 
         Schedule.setOnClickListener(view -> {
            Async execute=new Async();
            execute.execute();
-            progressBar.setVisibility(View.VISIBLE);
             String IQ=etext.getText().toString();
             DataBase.setSchedule(Utilities.Schedule(DataBase.Lessons(),DataBase.Teachers(),IQ));
 
@@ -37,6 +31,7 @@ public class IQActivity extends AppCompatActivity {
     }
     @SuppressLint("StaticFieldLeak")
     private final class Async extends AsyncTask<Void,Void,Void>{
+        private final ProgressDialog dialog = new ProgressDialog(IQActivity.this);
         @Override
         protected Void doInBackground(Void... voids) {
             String IQ = etext.getText().toString();
@@ -45,14 +40,15 @@ public class IQActivity extends AppCompatActivity {
         }
         @Override
         protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
+            dialog.show();
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
+            if(dialog.isShowing())
+                dialog.dismiss();
             Intent activityChangeIntent = new Intent(IQActivity.this, Final_Activity.class);
             startActivity(activityChangeIntent);
-            progressBar.setVisibility(View.INVISIBLE);
         }
     }
 
